50c800db29611619018213bc85bdd634d689072c
aleastChs <aleast@student.chalmers.se>
2018-07-26 13:47:34 +0200

    Commented stable version #4

--- js/src/main/scala/spgui/Main.scala
+++ js/src/main/scala/spgui/Main.scala
@@ -4,10 +4,9 @@ import org.scalajs.dom.document
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
-/**
-  * Created by alexa on 26/07/2018.
+/** Main class, initialized with scalaJSUseMainModuleInitializer := true
+  *
   */
-
 object Main {
   def main(args: Array[String]): Unit = {
     val Hello = ScalaComponent.builder[String]("Hello")
@@ -15,12 +14,5 @@ object Main {
       .build
 
     <.div(Hello("Frank")).renderIntoDOM(document.getElementById("root"))
-    /*val Hello =
-      ScalaComponent.builder[String]("Hello")
-        .render_P(name => <.div("Hello there ", name))
-        .build
-
-    // Usage:
-    <.div(Hello("Mathew")).renderIntoDOM(document.body)*/
   }
 }
